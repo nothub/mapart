4a41f654c5340d748e0b4a14605f640028a9334c
Florian Hübner <fhuebner@posteo.de>
2024-08-03 20:11:20 +0200

    notes

---
+++ fix.go
@@ -0,0 +1,30 @@
+package main
+
+func fixCmd(inputs []string) {
+
+	/*
+
+	   	   	   TODO
+
+	   	        nbt field: DataVersion (integer)
+	               The version the map was created.
+	               If not present, defaults to 1343 (1.12.2)
+
+	   	   	    switch id {
+	   	   	    case 0x00: //  0
+	   	   	        return "minecraft:overworld", true
+	   	   	    case 0xff: // -1
+	   	   	        return "minecraft:the_nether", true
+	   	   	    case 0x01: //  1
+	   	   	        return "minecraft:the_end", true
+	   	   	    default:
+	   	   	        return "", false
+	   	   	    }
+
+	   	   	    if m.Data.Dimension == "" {
+	   	   	        m.Data.Dimension = dimById(m.Data.DimensionOld)
+	   	   	    }
+
+	*/
+
+}
