f5b9af36d55734437ed8979b6a452b122472b499
Florian Hübner <fhuebner@posteo.de>
2024-08-03 22:38:31 +0200

    map nbt format converter

--- data.go
+++ data.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"bytes"
 	"compress/gzip"
 	"fmt"
 	"github.com/Tnze/go-mc/nbt"
+	"io"
 	"os"
 )
 
@@ -37,6 +39,7 @@ type Map1519 struct {
 		TrackingPosition  byte   `nbt:"trackingPosition"`
 		Colors            []byte `nbt:"colors"`
 	} `nbt:"data"`
+	Versioned
 }
 
 // 1.13.1
@@ -52,6 +55,7 @@ type Map1628 struct {
 		TrackingPosition  byte   `nbt:"trackingPosition"`
 		Colors            []byte `nbt:"colors"`
 	} `nbt:"data"`
+	Versioned
 }
 
 // 1.14
@@ -67,6 +71,7 @@ type Map1952 struct {
 		Locked            byte   `nbt:"locked"`
 		Colors            []byte `nbt:"colors"`
 	} `nbt:"data"`
+	Versioned
 }
 
 // 1.16
@@ -82,6 +87,7 @@ type Map2566 struct {
 		Locked            byte   `nbt:"locked"`
 		Colors            []byte `nbt:"colors"`
 	} `nbt:"data"`
+	Versioned
 }
 
 // 1.16.5
@@ -89,8 +95,8 @@ type Map2566 struct {
 // add field: UUIDLeast (long)
 type Map2586 struct {
 	Data struct {
-		UUIDMost          int    `nbt:"UUIDMost"`
-		UUIDLeast         int    `nbt:"UUIDLeast"`
+		UUIDMost          int64  `nbt:"UUIDMost"`
+		UUIDLeast         int64  `nbt:"UUIDLeast"`
 		XCenter           int    `nbt:"xCenter"`
 		ZCenter           int    `nbt:"zCenter"`
 		Scale             byte   `nbt:"scale"`
@@ -100,68 +106,172 @@ type Map2586 struct {
 		Locked            byte   `nbt:"locked"`
 		Colors            []byte `nbt:"colors"`
 	} `nbt:"data"`
+	Versioned
 }
 
-type Map Map2586
+type Map struct {
+	Data struct {
+		Width             int    `nbt:"width"`
+		Height            int    `nbt:"height"`
+		UUIDMost          int64  `nbt:"UUIDMost"`
+		UUIDLeast         int64  `nbt:"UUIDLeast"`
+		XCenter           int    `nbt:"xCenter"`
+		ZCenter           int    `nbt:"zCenter"`
+		Scale             byte   `nbt:"scale"`
+		Dimension         string `nbt:"dimension"`
+		UnlimitedTracking byte   `nbt:"unlimitedTracking"`
+		TrackingPosition  byte   `nbt:"trackingPosition"`
+		Locked            byte   `nbt:"locked"`
+		Colors            []byte `nbt:"colors"`
+	} `nbt:"data"`
+	Versioned
+}
 
 type Versioned struct {
 	DataVersion int `nbt:"DataVersion"`
 }
 
-func readData(version int) (m Map, err error) {
-	if version <= 1343 {
+func convert(b []byte) (m Map, err error) {
+
+	var v Versioned
+
+	_, err = nbt.NewDecoder(bytes.NewReader(b)).Decode(&v)
+	if err != nil {
+		return m, fmt.Errorf("failed to decode nbt data: %w", err)
+	}
+
+	if v.DataVersion <= 1343 {
 		var map1343 Map1343
-		_ = map1343
-		// TODO
-	}
-	if version <= 1519 {
+		_, err = nbt.NewDecoder(bytes.NewReader(b)).Decode(&map1343)
+		m.Data.Width = map1343.Data.Width
+		m.Data.Height = map1343.Data.Height
+		m.Data.UUIDMost = 0  // TODO
+		m.Data.UUIDLeast = 0 // TODO
+		m.Data.XCenter = map1343.Data.XCenter
+		m.Data.ZCenter = map1343.Data.ZCenter
+		m.Data.Scale = map1343.Data.Scale
+		m.Data.Dimension = dimById(int(map1343.Data.Dimension))
+		m.Data.UnlimitedTracking = map1343.Data.UnlimitedTracking
+		m.Data.TrackingPosition = map1343.Data.TrackingPosition
+		m.Data.Locked = 0
+		m.Data.Colors = map1343.Data.Colors
+		m.DataVersion = 1343
+
+	} else if v.DataVersion <= 1519 {
 		var map1519 Map1519
-		_ = map1519
-		// TODO
-	}
-	if version <= 1628 {
+		_, err = nbt.NewDecoder(bytes.NewReader(b)).Decode(&map1519)
+		m.Data.Width = 128
+		m.Data.Height = 128
+		m.Data.UUIDMost = 0  // TODO
+		m.Data.UUIDLeast = 0 // TODO
+		m.Data.XCenter = map1519.Data.XCenter
+		m.Data.ZCenter = map1519.Data.ZCenter
+		m.Data.Scale = map1519.Data.Scale
+		m.Data.Dimension = dimById(int(map1519.Data.Dimension))
+		m.Data.UnlimitedTracking = map1519.Data.UnlimitedTracking
+		m.Data.TrackingPosition = map1519.Data.TrackingPosition
+		m.Data.Locked = 0
+		m.Data.Colors = map1519.Data.Colors
+		m.DataVersion = map1519.DataVersion
+
+	} else if v.DataVersion <= 1628 {
 		var map1628 Map1628
-		_ = map1628
-		// TODO
-	}
-	if version <= 1952 {
+		_, err = nbt.NewDecoder(bytes.NewReader(b)).Decode(&map1628)
+		m.Data.Width = 128
+		m.Data.Height = 128
+		m.Data.UUIDMost = 0  // TODO
+		m.Data.UUIDLeast = 0 // TODO
+		m.Data.XCenter = map1628.Data.XCenter
+		m.Data.ZCenter = map1628.Data.ZCenter
+		m.Data.Scale = map1628.Data.Scale
+		m.Data.Dimension = dimById(map1628.Data.Dimension)
+		m.Data.UnlimitedTracking = map1628.Data.UnlimitedTracking
+		m.Data.TrackingPosition = map1628.Data.TrackingPosition
+		m.Data.Locked = 0
+		m.Data.Colors = map1628.Data.Colors
+		m.DataVersion = map1628.DataVersion
+
+	} else if v.DataVersion <= 1952 {
 		var map1952 Map1952
-		_ = map1952
-		// TODO
-	}
-	if version <= 2566 {
+		_, err = nbt.NewDecoder(bytes.NewReader(b)).Decode(&map1952)
+		m.Data.Width = 128
+		m.Data.Height = 128
+		m.Data.UUIDMost = 0  // TODO
+		m.Data.UUIDLeast = 0 // TODO
+		m.Data.XCenter = map1952.Data.XCenter
+		m.Data.ZCenter = map1952.Data.ZCenter
+		m.Data.Scale = map1952.Data.Scale
+		m.Data.Dimension = dimById(map1952.Data.Dimension)
+		m.Data.UnlimitedTracking = map1952.Data.UnlimitedTracking
+		m.Data.TrackingPosition = map1952.Data.TrackingPosition
+		m.Data.Locked = map1952.Data.Locked
+		m.Data.Colors = map1952.Data.Colors
+		m.DataVersion = map1952.DataVersion
+
+	} else if v.DataVersion <= 2566 {
 		var map2566 Map2566
-		_ = map2566
-		// TODO
-	}
-	if version <= 2586 {
+		_, err = nbt.NewDecoder(bytes.NewReader(b)).Decode(&map2566)
+		m.Data.Width = 128
+		m.Data.Height = 128
+		m.Data.UUIDMost = 0  // TODO
+		m.Data.UUIDLeast = 0 // TODO
+		m.Data.XCenter = map2566.Data.XCenter
+		m.Data.ZCenter = map2566.Data.ZCenter
+		m.Data.Scale = map2566.Data.Scale
+		m.Data.Dimension = map2566.Data.Dimension
+		m.Data.UnlimitedTracking = map2566.Data.UnlimitedTracking
+		m.Data.TrackingPosition = map2566.Data.TrackingPosition
+		m.Data.Locked = map2566.Data.Locked
+		m.Data.Colors = map2566.Data.Colors
+		m.DataVersion = map2566.DataVersion
+
+	} else if v.DataVersion <= 2586 {
 		var map2586 Map2586
-		_ = map2586
-		// TODO
+		_, err = nbt.NewDecoder(bytes.NewReader(b)).Decode(&map2586)
+		m.Data.Width = 128
+		m.Data.Height = 128
+		m.Data.UUIDMost = map2586.Data.UUIDMost
+		m.Data.UUIDLeast = map2586.Data.UUIDLeast
+		m.Data.XCenter = map2586.Data.XCenter
+		m.Data.ZCenter = map2586.Data.ZCenter
+		m.Data.Scale = map2586.Data.Scale
+		m.Data.Dimension = map2586.Data.Dimension
+		m.Data.UnlimitedTracking = map2586.Data.UnlimitedTracking
+		m.Data.TrackingPosition = map2586.Data.TrackingPosition
+		m.Data.Locked = map2586.Data.Locked
+		m.Data.Colors = map2586.Data.Colors
+		m.DataVersion = map2586.DataVersion
+
+	} else {
+		err = fmt.Errorf("unsupported data version %v", v.DataVersion)
 	}
+
+	if err != nil {
+		return m, fmt.Errorf("failed to decode nbt data (data version %v): %w", v.DataVersion, err)
+	}
+
 	return m, nil
 }
 
-func LoadMap(path string) (*Map, error) {
+func LoadMap(path string) (m Map, err error) {
 
 	fr, err := os.Open(path)
 	if err != nil {
-		return nil, fmt.Errorf("failed to open file: %w", err)
+		return m, fmt.Errorf("failed to open file: %w", err)
 	}
 	defer fr.Close()
 
 	// map files are always gzipped
 	gzr, err := gzip.NewReader(fr)
 	if err != nil {
-		return nil, fmt.Errorf("failed to decompress gzip: %w", err)
+		return m, fmt.Errorf("failed to decompress gzip: %w", err)
 	}
 
-	var m Map
-
-	_, err = nbt.NewDecoder(gzr).Decode(&m)
+	var b []byte
+	_, err = io.ReadFull(gzr, b)
 	if err != nil {
-		return nil, fmt.Errorf("failed to decode nbt data: %w", err)
+		return m, fmt.Errorf("failed to read data: %w", err)
 	}
 
-	return &m, nil
+	return convert(b)
 }
--- fix.go
+++ fix.go
@@ -1,30 +1,18 @@
 package main
 
 func fixCmd(inputs []string) {
+	// TODO
+}
 
-	/*
-
-	   	   	   TODO
-
-	   	        nbt field: DataVersion (integer)
-	               The version the map was created.
-	               If not present, defaults to 1343 (1.12.2)
-
-	   	   	    switch id {
-	   	   	    case 0x00: //  0
-	   	   	        return "minecraft:overworld", true
-	   	   	    case 0xff: // -1
-	   	   	        return "minecraft:the_nether", true
-	   	   	    case 0x01: //  1
-	   	   	        return "minecraft:the_end", true
-	   	   	    default:
-	   	   	        return "", false
-	   	   	    }
-
-	   	   	    if m.Data.Dimension == "" {
-	   	   	        m.Data.Dimension = dimById(m.Data.DimensionOld)
-	   	   	    }
-
-	*/
-
+func dimById(id int) string {
+	switch id {
+	case 0:
+		return "minecraft:overworld"
+	case -1:
+		return "minecraft:the_nether"
+	case 1:
+		return "minecraft:the_end"
+	default:
+		return "minecraft:overworld"
+	}
 }
